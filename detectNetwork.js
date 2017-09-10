c9255573c18742751892d548ea27523050e7852e
Carter Duncan <dukeblue2017@gmail.com>
2017-09-09 21:19:09 -0400

    All cards are successfully identified, not all tests have been created yet

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,30 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if (cardNumber.length === 15 && (cardNumber.startsWith('34') || cardNumber.startsWith('37'))) {
+    return 'American Express';
+  }
+  if (cardNumber.length === 14 && (cardNumber.startsWith('38') || cardNumber.startsWith('39'))) {
+  	return 'Diner\'s Club';
+  }
+  if (
+  	cardNumber.startsWith('4') &&
+  	(cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)
+  	) {
+  	return 'Visa';
+  }
+  var firstTwo = cardNumber.substring(0,2)*1;
+  if (cardNumber.length === 16 && firstTwo >= 51 && firstTwo <= 55) {
+  	return 'MasterCard';
+  }
+  if ( (cardNumber.length === 16 || cardNumber.length === 19) && cardNumber.startsWith('6011')) {
+  	return 'Discover';
+  }
+  var firstFour = cardNumber.substring(0,4);
+  console.log(cardNumber.length)
+  if (cardNumber.length >= 12 && cardNumber.length <= 19) {
+    if (firstFour === '5018' || firstFour === '5020' || firstFour === '5038' || firstFour === '6304') {
+    	return 'Maestro'
+    }
+  }
 };
-
-
